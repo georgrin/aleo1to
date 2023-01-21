ae516012357e2fc019747cb60d33a9d1fbfbf55e
Eugene Butkov <eugenefromrus@gmail.com>
2023-01-21 13:39:48 +1000

    Add timeout srver to 300000ms

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,7 @@ export default defineConfig({
       '/api': {
         target: 'https://api.aleo1.to/v1/',
         changeOrigin: true,
+        timeout: 30000,
         rewrite: (path) => path.replace(/^\/api/, ''),
       }
     }
