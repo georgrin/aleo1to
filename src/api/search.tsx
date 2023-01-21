a0b3e70ed782f1911b7c13ad7a1f4feab45896c2
Eugene Butkov <eugenefromrus@gmail.com>
2023-01-21 13:13:44 +1000

    fix: Remove delay before api call

--- src/api/search.tsx
+++ src/api/search.tsx
@@ -1,7 +1,5 @@
 import axios from 'axios';
-import { waitTime } from '../helpers/waitTime';
 
 export async function searchAddress(address: string) {
-    await waitTime(300)
     return axios.get(`/api/wallets/${address}`).then(request => request.data);
 }
--- src/helpers/waitTime.ts
+++
@@ -1,5 +0,0 @@
-const waitTime = (ms:number) => {
-    return new Promise(resolve => setTimeout(resolve, ms));
-}
-
-export {waitTime}
