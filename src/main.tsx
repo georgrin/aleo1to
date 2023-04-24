eb2cb3e34ed49bf91341c0d7d0cc8e113a31ed61
Eugene Butkov <eugenefromrus@gmail.com>
2023-04-24 20:16:44 +1000

    feat: add @sentry/react

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,12 @@
-// TODO: Unused import
-import React from "react";
+import * as Sentry from "@sentry/react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+
+Sentry.init({
+  dsn: "https://66b0e1921ac6417083c93c0c4d77c3d1@sentry.nod.run/6",
+  integrations: [new Sentry.BrowserTracing()],
+  tracesSampleRate: 1.0, // Capture 100% of the transactions, reduce in production!
+});
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <App />
--- src/pages/Home/Footer.tsx
+++ src/pages/Home/Footer.tsx
@@ -1,18 +1,21 @@
+import { useEffect } from "react";
+
 export const Footer = (props: any) => {
+  useEffect(() => {
+    try {
+      throw Error("This is an example error on Footer");
+    } catch (error) {
+      console.log('error message: ', error);
+    }
+  }, []);
   return (
     <>
       <div className="h-14 md:h-10"></div>
       <div className="fixed flex items-center bottom-0 w-full h-auto md:h-10 bg-surface top-line font-thirdly text-sm">
         <div className="container flex items-center justify-end">
           {/* <div className="mr-auto text-[12px]">1to (c) 2023</div> */}
-          <a
-            target="_blank"
-            href="https://www.ingonyama.com"
-            className="flex items-center"
-          >
-            <div className="relative sm:mt-[4px] mr-[10px] text-[12px]">
-              Built with:
-            </div>
+          <a target="_blank" href="https://www.ingonyama.com" className="flex items-center">
+            <div className="relative sm:mt-[4px] mr-[10px] text-[12px]">Built with:</div>
             <span className="inline-block top-[2px] icon logo-footer-icon w-[90px] h-[25px] sm:w-[125px] sm:h-[30px]"></span>
           </a>
         </div>
