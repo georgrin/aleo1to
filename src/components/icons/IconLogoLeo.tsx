6ea31ddb1703fedf8fa2bcc3023edc6a19fb5507
Azat Kashapov <azat.developer@gmail.com>
2023-10-26 20:25:48 +0200

    add wallet modal to searchresult

---
+++ src/components/icons/IconLogoLeo.tsx
@@ -0,0 +1,35 @@
+export const IconLogoLeo = (props: any) => {
+  return (
+    <>
+      <svg
+        width='24'
+        height='24'
+        viewBox='0 0 24 24'
+        fill='none'
+        xmlns='http://www.w3.org/2000/svg'
+      >
+        <path d='M0 0H24V24H0V0Z' fill='white' />
+        <path
+          fillRule='evenodd'
+          clipRule='evenodd'
+          d='M22.099 1.90099H1.90099V22.099H22.099V1.90099ZM0 0V24H24V0H0Z'
+          fill='#7F31FF'
+        />
+        <path
+          fillRule='evenodd'
+          clipRule='evenodd'
+          d='M14.7327 9.26733H9.26733V14.7327H14.7327V9.26733ZM7.36634 7.36634V16.6337H16.6337V7.36634H7.36634Z'
+          fill='#7F31FF'
+        />
+        <path
+          d='M7.36634 1.90099H9.26733V7.36634H7.36634V1.90099Z'
+          fill='#7F31FF'
+        />
+        <path
+          d='M14.7327 16.6337H16.6337V22.099H14.7327V16.6337Z'
+          fill='#7F31FF'
+        />
+      </svg>
+    </>
+  );
+};
