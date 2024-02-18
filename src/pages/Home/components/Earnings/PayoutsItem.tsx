12f26b970b768ee18b36269e53823ae9f3ee5558
StanleySB <alexandervelesyuk@gmail.com>
2024-02-18 11:28:05 +0200

    link fix

--- src/pages/Home/components/Earnings/PayoutsItem.tsx
+++ src/pages/Home/components/Earnings/PayoutsItem.tsx
@@ -3,7 +3,13 @@ import { formatTimestampToTime } from "../../../../helpers/time";
 import { Payouts } from "../../../../model";
 import { StatusMap } from "../../hooks/useSearchDateSummary";
 
-const PayoutsGridItem = ({ amount, fee, status, created_at }: Payouts) => (
+const PayoutsGridItem = ({
+  amount,
+  fee,
+  status,
+  created_at,
+  txid,
+}: Payouts) => (
   <div className="grid earnings-grid w-full text-white py-2 px-6 text-xs bg-[#00FFAB]/[0.05]">
     <p className="pl-5">{formatTimestampToTime(created_at)}</p>
     {Array(6)
@@ -16,7 +22,16 @@ const PayoutsGridItem = ({ amount, fee, status, created_at }: Payouts) => (
     <p>{n(adjustNumber(fee || 0, 6))}</p>
     <p className={status === "PENDING" ? "text-[#F7A328]" : ""}>
       {StatusMap[status as keyof typeof StatusMap]}
-      <span className="text-grey">(tx)</span>
+      {status === "SENT" ||
+        (status === "DONE" && (
+          <a
+            target="_blank"
+            href={`https://explorer.aleo.org/transaction/${txid}`}
+            className="text-grey cursor-pointer"
+          >
+            &nbsp;(tx)
+          </a>
+        ))}
     </p>
   </div>
 );
