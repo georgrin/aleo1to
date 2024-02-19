610c659e8ba45ee8a222010269117a9dd84b6d69
StanleySB <alexandervelesyuk@gmail.com>
2024-02-19 16:42:40 +0200

    updates

--- src/pages/TestnetRewards/components/NewRewardTable.tsx
+++ src/pages/TestnetRewards/components/NewRewardTable.tsx
@@ -1,5 +1,6 @@
 import { testnet3Payout } from "../../../api/testnet";
 import WalletSign from "../../../components/WalletSign";
+import RequestedSign from "../../../components/WalletSign/RequestedSign";
 import SuccessSign from "../../../components/WalletSign/SuccessSign";
 import { IconCheckSmall } from "../../../components/icons/IconCheckSmall";
 import { Testnet3, TestnetStatus } from "../../../model/Testnet";
@@ -15,24 +16,9 @@ const PROVING_ROWS = [
     filedName: "testnet_mainnet_rate",
     main: false,
   },
-  {
-    name: "Mainnet credits (3000 / 2,5)",
-    filedName: "mainnet_credits",
-    main: true,
-  },
 ];
 
 const VALIDATOR_BONUS_ROWS = [
-  {
-    name: "Total testnet credits earned by all users",
-    filedName: "total_credits_earned_by_all_users",
-    main: false,
-  },
-  {
-    name: "Your share of total credits (3000 / 2700000)",
-    filedName: "address_share_of_total_credits",
-    main: false,
-  },
   {
     name: "10% of validator phase total reward",
     filedName: "validator_phase_bonus_10",
@@ -50,6 +36,21 @@ interface Props {
   data: Testnet3;
 }
 
+const RewardRow = ({
+  text,
+  data,
+  main,
+}: {
+  text: string;
+  data: string | number;
+  main: boolean;
+}) => (
+  <div className="flex justify-between bottom-line mb-[-1px] py-[8px]">
+    <p className={!main ? "text-default" : ""}>{text}</p>
+    <p className={main ? "text-secondary" : ""}>{data}</p>
+  </div>
+);
+
 const NewPhaseRewardTable = ({ address, data }: Props) => {
   return (
     <>
@@ -60,28 +61,36 @@ const NewPhaseRewardTable = ({ address, data }: Props) => {
         <div>
           <p className="mb-[8px]">Proving participation rewards</p>
           {PROVING_ROWS.map(({ name, filedName, main }) => (
-            <div
-              className="flex justify-between bottom-line mb-[-1px] py-[8px]"
+            <RewardRow
               key={name}
-            >
-              <p className={!main ? "text-default" : ""}>{name}</p>
-              <p className={main ? "text-secondary" : ""}>
-                {data[filedName as keyof Testnet3]}
-              </p>
-            </div>
+              data={data[filedName as keyof Testnet3]}
+              text={name}
+              main={main}
+            />
           ))}
-
+          <RewardRow
+            data={data.mainnet_credits}
+            text={`Mainnet credits (${data.testnet_credits} / ${data.testnet_mainnet_rate})`}
+            main={true}
+          />
           <p className="mt-4 mb-[8px]">Validator phase bonus</p>
+          <RewardRow
+            data={data.total_credits_earned_by_all_users}
+            text="Total testnet credits earned by all users"
+            main={false}
+          />
+          <RewardRow
+            data={data.address_share_of_total_credits}
+            text={`Your share of total credits (${data.testnet_credits} / ${data.total_credits_earned_by_all_users})`}
+            main={false}
+          />
           {VALIDATOR_BONUS_ROWS.map(({ name, filedName, main }) => (
-            <div
-              className="flex justify-between bottom-line mb-[-1px] py-[8px]"
+            <RewardRow
               key={name}
-            >
-              <p className={!main ? "text-default" : ""}>{name}</p>
-              <p className={main ? "text-secondary" : ""}>
-                {data[filedName as keyof Testnet3]}
-              </p>
-            </div>
+              data={data[filedName as keyof Testnet3]}
+              text={name}
+              main={main}
+            />
           ))}
         </div>
         <div className="flex justify-between items-center py-[8px]">
@@ -97,19 +106,14 @@ const NewPhaseRewardTable = ({ address, data }: Props) => {
           />
         )}
         {data.status === TestnetStatus.REQUESTED && (
-          <div className="text-xs">
-            <div className="border border-primary rounded flex justify-between items-center w-full py-[10px] pr-[6px] px-4">
-              <div className="flex items-center justify-center w-full">
-                <span className="mr-1">
-                  <IconCheckSmall />
-                </span>
-                <span className="text-default">Requested...</span>
-              </div>
-            </div>
+          <div className="mt-auto">
+            <RequestedSign />
           </div>
         )}
         {data.status === TestnetStatus.SENT && (
-          <SuccessSign publicKey={address} />
+          <div className="mt-auto">
+            <SuccessSign publicKey={address} />
+          </div>
         )}
       </div>
     </>
