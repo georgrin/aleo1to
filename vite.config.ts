ab78a1ab713517561922992747468279dd112a4f
StanleySB <alexandervelesyuk@gmail.com>
2024-02-19 19:09:08 +0200

    test commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,16 @@
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default ({ mode }) => {
+  // Load environment variables
   process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+
   return defineConfig({
     plugins: [react()],
     server: {
       proxy: {
         "/api": {
-          target: process.env.VITE_API_URL,
+          target: process.env.VITE_API_URL || "https://api.aleo1.to/v1",
           changeOrigin: true,
           timeout: 1000,
           rewrite: (path) => path.replace(/^\/api/, ""),
@@ -21,4 +22,3 @@ export default ({ mode }) => {
     },
   });
 };
-
