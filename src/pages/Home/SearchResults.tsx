3782079c8fe76d166e72d96ca6e6cf1919e1690b
georgrin <80432064+georgrin@users.noreply.github.com>
2024-09-05 02:54:09 +0300

    Update SearchResults.tsx

--- src/pages/Home/SearchResults.tsx
+++ src/pages/Home/SearchResults.tsx
@@ -63,7 +63,7 @@ function SearchResult({ searchResult, deleteSearchResult }: SearchResultProps) {
             fees={general_info.fee_total}
             balance={general_info.balance}
             autoPayout={general_info.auto_payout}
-            incentivize={balance.balance.in_pool_incentivize.total}
+            incentivize={0}
           />
           <EarningsGrid earnings={earnings} payouts={payouts} />
         </>
